4c24f4e86eeac2dfaf0a48f32adc72042fe7ef2d
alessandropisent <alessandro.pisent@hotmail.com>
2023-10-23 16:58:56 +0200

    Added bash script to download and start everything

---
+++ start.sh
@@ -0,0 +1,46 @@
+# Clone and Start Repos
+
+### Clone Gatway
+git clone https://github.com/giovdellap/NFFH-gateway.git
+cd NFFH-gateway 
+docker compose up &
+cd
+
+## Clone Auth
+git clone https://github.com/alessandropisent/NFFH-Auth.git
+cd NFFH-Auth
+docker compose up &
+cd
+
+## Clone Image Server
+git clone https://github.com/alessandropisent/NFFH-ImageServer.git
+cd NFFH-ImageServer
+docker compose up &
+cd
+
+## Clone Farmer BE
+git clone https://github.com/IvanNic06/NFFH-Farmer-BE.git
+### Install
+cd NFFH-Farmer-BE
+mvn install
+docker compose up &
+cd ..
+
+## Clone Client BE
+git clone https://github.com/IvanNic06/NFFH-Client-BE.git
+### Install
+cd NFFH-Client-BE
+mvn install
+docker compose up &
+cd ..
+
+## Clone Order BE
+git clone https://github.com/IvanNic06/NFFH-Order-BE.git
+### Install
+cd NFFH-Order-BE
+mvn install
+docker compose up &
+cd ..
+
+## Clone Farmer FE
+#git clone https://github.com/giovdellap/NFFH-client-FE.git
